2efeba2b74d44fded728bfd956822ba7eb7ce917
WoundedMighty212 <michael.ludick13@outlook.com>
2024-08-27 11:59:34 +0200

    added custom user and roles tables and configutred them in ef core

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UserRolesMaps.Models;
 
 namespace UserRolesMaps.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
