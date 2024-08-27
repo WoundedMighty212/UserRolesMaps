bbb8eb02cd6f7ecacdf7b50b8dbf9964be529e98
WoundedMighty212 <michael.ludick13@outlook.com>
2024-08-27 15:11:47 +0200

    db migration

--- Data/Migrations/20240827131013_NewTables.Designer.cs
+++ Data/Migrations/20240827131013_NewTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UserRolesMaps.Data;
 
@@ -11,9 +12,11 @@ using UserRolesMaps.Data;
 namespace UserRolesMaps.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240827131013_NewTables")]
+    partial class NewTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
