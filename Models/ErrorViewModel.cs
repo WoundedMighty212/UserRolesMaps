4d420004ca487260388d05ec28b4624bfbe34eb9
WoundedMighty212 <michael.ludick13@outlook.com>
2024-08-27 11:46:50 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserRolesMaps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
