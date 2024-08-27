e3731706ea72cc594f694ddcfc242be8ff0b6ed5
WoundedMighty212 <michael.ludick13@outlook.com>
2024-08-27 15:03:12 +0200

    new models

---
+++ Models/Catagory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace UserRolesMaps.Models
+{
+    public class Catagory
+    {
+        public virtual int CatId { get; set; }
+        public virtual string CatDescription { get; set; }
+        public virtual List<Events> MyEvents { get; set; }
+    }
+}
---
+++ Models/Events.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace UserRolesMaps.Models
+{
+    public class Events
+    {
+        public virtual int EventId { get; set; }
+        public virtual string UserId { get; set; }
+        public virtual string EventLat { get; set; }
+        public virtual string EventLong { get; set; }
+        public virtual string EventURL { get; set; }
+        public virtual DateTime EventDate { get; set; }
+        public virtual int CatId { get; set; }
+        public virtual Catagory EventCategory { get; set; }
+    }
+}
